7bfb0f3478be8e01bdf78bd1528d44deb61db4ba
wzshiming <wzshiming@foxmail.com>
2018-05-11 16:42:18 +0800

    add out profile

--- cmd/gs/main.go
+++ cmd/gs/main.go
@@ -3,12 +3,27 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"runtime/pprof"
 
 	"github.com/wzshiming/gs/exec"
 )
 
-func main() {
+var profile = flag.String("profile", "", "profile file path")
+
+func init() {
 	flag.Parse()
+}
+
+func main() {
+
+	if *profile != "" {
+		f, _ := os.Create(*profile)
+		defer f.Close()
+		pprof.StartCPUProfile(f)     // 开始cpu profile，结果写到文件f中
+		defer pprof.StopCPUProfile() // 结束profile
+	}
+
 	e := exec.NewExec()
 	for _, v := range flag.Args() {
 		v, err := e.File(v)
